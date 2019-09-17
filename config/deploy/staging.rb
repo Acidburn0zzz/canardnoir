3bd7046e7d6cb8668192fa48abad04f436a691dd
Paddy <rapbhan@users.noreply.github.com>
2019-09-17 18:08:49 +0530

    Updated the Staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,29 +1,53 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-role :web_1, %w[serv-deployer@oh-web01.dc1.lan]
-role :web_2, %w[serv-deployer@oh-web02.dc1.lan]
-role :web_3, %w[serv-deployer@oh-web03.dc1.lan]
+# role :web_1, %w[serv-deployer@oh-web01.dc1.lan]
+# role :web_2, %w[serv-deployer@oh-web02.dc1.lan]
+# role :web_3, %w[serv-deployer@oh-web03.dc1.lan]
 
-role :web, ['serv-deployer@oh-web01.dc1.lan', 'serv-deployer@oh-web02.dc1.lan', 'serv-deployer@oh-web03.dc1.lan']
+# role :web, ['serv-deployer@oh-web01.dc1.lan', 'serv-deployer@oh-web02.dc1.lan', 'serv-deployer@oh-web03.dc1.lan']
 
-role :selenium, %w[serv-deployer@oh-web03.dc1.lan]
+# role :selenium, %w[serv-deployer@oh-web03.dc1.lan]
 
-role :db, %w[serv-deployer@oh-utility01.dc1.lan], primary: true
+# role :db, %w[serv-deployer@oh-utility01.dc1.lan], primary: true
 
-role :utility, %w[serv-deployer@oh-utility01.dc1.lan]
+# role :utility, %w[serv-deployer@oh-utility01.dc1.lan]
 
-set :user, 'serv-deployer'
-set :default_env, 'PATH' => '/home/serv-deployer/.rbenv/shims:$PATH',
-                  'BASH_ENV' => '/home/serv-deployer/.production_vars'
+# set :user, 'serv-deployer'
+# set :default_env, 'PATH' => '/home/serv-deployer/.rbenv/shims:$PATH',
+#                   'BASH_ENV' => '/home/serv-deployer/.production_vars'
 
-# All passenger_roles get a deploy:restart after deploy:publishing.
-set :passenger_roles, %i[web web_1 web_2 web_3 web_4 web_5 web_7 web_8]
-set :sidekiq_role, [:utility]
-set :rails_env, 'staging'
+# # All passenger_roles get a deploy:restart after deploy:publishing.
+# set :passenger_roles, %i[web web_1 web_2 web_3 web_4 web_5 web_7 web_8]
+# set :sidekiq_role, [:utility]
+# set :rails_env, 'staging'
 
-# shared/.env.staging contains environment specific dotenv overrides.
-set :linked_files, %w[.env.staging]
+# # shared/.env.staging contains environment specific dotenv overrides.
+# set :linked_files, %w[.env.staging]
 
-set sidekiq_env: fetch(:rails_env)
-set :assets_roles, %i[web_1 web_2 web_3 web_4 web_5 web_6 web_7 web_8]
-set :passenger_restart_with_touch, true
+# set sidekiq_env: fetch(:rails_env)
+# set :assets_roles, %i[web_1 web_2 web_3 web_4 web_5 web_6 web_7 web_8]
+# set :passenger_restart_with_touch, true
+
+
+#Pulls the latest docker image on all the 3 web heads
+on ['serv-deployer@oh-web01.dc1.lan', 'serv-deployer@oh-web02.dc1.lan', 'serv-deployer@oh-web03.dc1.lan'] do |host|
+  within '/home/serv-deployer' do
+    execute('wget https://raw.githubusercontent.com/blackducksoftware/ohloh-ui/master/docker-compose.yml')
+    execute('docker pull sigsynopsys/openhub:latest')
+  end
+end
+
+#Run the bundle install, assets on one of the web host
+on ["serv-deployer@oh-web01.dc1.lan"] do |host|
+  within '/home/serv-deployer' do
+    execute('docker-compose run --rm web bundle install')
+    execute('docker-compose run --rm web rake assets:precompile RAILS_ENV=staging')
+  end
+end
+
+#Rebuild the container with the latest changes (code, assets)
+on ['serv-deployer@oh-web01.dc1.lan', 'serv-deployer@oh-web02.dc1.lan', 'serv-deployer@oh-web03.dc1.lan'] do |host|
+  within '/home/serv-deployer' do
+    execute('docker-compose up -d --build')
+  end
+end
